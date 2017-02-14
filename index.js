1c3ad6b2e5f3d9fc024dd20cda1ad1e6d96aecf8
kreatimont <nadtoka.alexandr@gmail.com>
2017-02-14 21:47:42 +0200

    add nodejs

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.disable('x-powered-by')
+
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/', function(req, res) {
+	res.redirect('/public');
+})
+
+
+const server = app.listen(1337, function() {
+	console.log('Listening on \"localhost:1337\"')
+})
