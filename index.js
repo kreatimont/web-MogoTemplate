cd81882c9fa179008cb0d810d1c21806964348e0
kreatimont <nadtoka.alexandr@gmail.com>
2017-02-27 20:43:35 +0000

    fix index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.disable('x-powered-by')
+app.disable('x-powered-by');
 
-app.use('/public', express.static(__dirname + '/public'))
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	res.redirect('/public');
-})
+});
 
 
 const server = app.listen(1337, function() {
-	console.log('Listening on \"localhost:1337\"')
-})
+	console.log('Listening on \"localhost:1337\"');
+});
+	
