9040573180ce2bbf15ba749cf527cf401c1e84a0
kreatimont <nadtoka.alexandr@gmail.com>
2017-05-15 16:32:37 +0300

    add pusher

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const Pusher = require('pusher');
 
 app.disable('x-powered-by');
 
@@ -9,6 +10,22 @@ app.get('/', function(req, res) {
 	res.redirect('/public');
 });
 
+var pusher = new Pusher({
+  appId: '339425',
+  key: '34e3a6454e1f36306bfd',
+  secret: '66846ab7a1f5c7abc67b',
+  cluster: 'eu',
+  encrypted: true
+});
+
+var postMessage = function() {
+	console.log('postMessage...');
+	pusher.trigger('my-channel', 'my-event', {
+  		"message": "hello world"
+	});
+}
+
+setInterval(postMessage, 5000);
 
 const server = app.listen(1337, function() {
 	console.log('Listening on \"localhost:1337\"');
